aa9b51b02b71200678dffaa137aea5a74e201ea8
Kamil Wawer <Kamil.wawer92@gmail.com>
2019-02-19 20:33:54 +0100

    Add simplest login form with LoginServlet

---
+++ src/main/java/com/infoshare/academy/servlets/LoginServlet.java
@@ -0,0 +1,35 @@
+package com.infoshare.academy.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if(username.isEmpty() || password.isEmpty() )
+        {
+            out.println("Username or Password incorrect");
+            RequestDispatcher rs = request.getRequestDispatcher("login.jsp");
+            rs.include(request, response);
+
+        }
+        else
+        {
+            RequestDispatcher req = request.getRequestDispatcher("Welcome.jsp");
+            req.forward(request, response);
+        }
+    }
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <title>Login Form</title>
+</head>
+<body>
+<form action="/jjdzl1-css/LoginServlet" method="post">
+    <table style="with: 50%">
+
+        <tr>
+            <td>UserName</td>
+            <td><input type="text" name="username" /></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><input type="password" name="password" /></td>
+        </tr>
+    </table>
+    <input type="submit" value="Login" /></form>
+</body>
+</html>
