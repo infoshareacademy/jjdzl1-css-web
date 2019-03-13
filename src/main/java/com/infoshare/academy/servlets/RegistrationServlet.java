7c11eca9f37fc312b6613eec532cff98f5efa7fc
Łukasz Waręcki <glenski2be7@gmail.com>
2019-03-13 20:27:51 +0100

    Move static voids from RegistrationServlet to RegistrationMessages class

--- src/main/java/com/infoshare/academy/servlets/RegistrationServlet.java
+++ src/main/java/com/infoshare/academy/servlets/RegistrationServlet.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
 
+import static com.infoshare.academy.utils.RegistrationMessages.*;
+
 @WebServlet("/register")
 public class RegistrationServlet extends HttpServlet {
 
@@ -86,51 +88,4 @@ public class RegistrationServlet extends HttpServlet {
         }
     }
 
-    private static String passwordIncorrectMessage() {
-        String html1 = "<div class=\"alert alert-danger\" role=\"alert\">";
-        String html2 = "</div>";
-        String errorData = "Password too weak! Use at least 8 characters (one digit, letter and special character)";
-        return html1 + errorData + html2;
-    }
-
-    private static String tooYoungMessage() {
-        String html1 = "<div class=\"alert alert-danger\" role=\"alert\">";
-        String html2 = "</div>";
-        String errorData = "You are too young to register account!";
-        return html1 + errorData + html2;
-    }
-
-    private static String passwordIncorrectAndTooYoungMessage() {
-        String html1 = "<div class=\"alert alert-danger\" role=\"alert\">";
-        String html2 = "</div>";
-        String errorDataTooYoung = "You are too young to register account!";
-        String html3 = "<div class=\"alert alert-danger\" role=\"alert\">";
-        String html4 = "</div>";
-        String errorDataIncorrectPassword = "Password too weak! Use at least 8 characters (one digit, letter and special character)";
-        return html1 + errorDataTooYoung + html2 + html3 + errorDataIncorrectPassword + html4;
-    }
-
-    private static String unavailableLogin() {
-        String html1 = "<div class=\"alert alert-danger\" role=\"alert\">";
-        String html2 = "</div>";
-        String errorData = "This login is unavailable! Please, try another one.";
-        return html1 + errorData + html2;
-    }
-
-    private static String unavailableEmail() {
-        String html1 = "<div class=\"alert alert-danger\" role=\"alert\">";
-        String html2 = "</div>";
-        String errorData = "This email is unavailable! Please, try another one.";
-        return html1 + errorData + html2;
-    }
-
-    private static String loginAndEmailIsUnavailable() {
-        String html1 = "<div class=\"alert alert-danger\" role=\"alert\">";
-        String html2 = "</div>";
-        String errorDataTooYoung = "This login is unavailable! Please, try another one.";
-        String html3 = "<div class=\"alert alert-danger\" role=\"alert\">";
-        String html4 = "</div>";
-        String errorDataIncorrectPassword = "This email is unavailable! Please, try another one.";
-        return html1 + errorDataTooYoung + html2 + html3 + errorDataIncorrectPassword + html4;
-    }
 }
