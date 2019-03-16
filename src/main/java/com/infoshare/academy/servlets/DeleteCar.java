98898172d0c46f2faa70e656c32e4c7ffc7fc1e0
arexe <arexepro@gmail.com>
2019-03-16 12:37:44 +0100

    Fixes to DeleteCar servlet

--- src/main/java/com/infoshare/academy/servlets/DeleteCar.java
+++ src/main/java/com/infoshare/academy/servlets/DeleteCar.java
@@ -22,48 +22,44 @@ public class DeleteCar extends HttpServlet {
 
         String id = req.getParameter("id");
 
-        if (id.isEmpty()) {
+        if (id == null) {
+            req.getRequestDispatcher("/admin/deleteCar.jsp").forward(req, resp);
+        } else if (id.isEmpty()) {
             req.setAttribute("error", errorMessage());
-        }
-
-        Car car = dao.getCar(Integer.valueOf(id));
-
-        if (car != null) {
-            req.setAttribute("id", car.getId());
-            req.setAttribute("make", car.getMake());
-            req.setAttribute("model", car.getModel());
-            req.setAttribute("year", car.getYear());
-
+            req.getRequestDispatcher("/admin/deleteCar.jsp").forward(req, resp);
         } else {
-            req.setAttribute("error", errorMessage());
+            Car car = dao.getCar(Integer.valueOf(id));
+            if (car != null) {
+                req.setAttribute("car", car);
+            }
+            req.getRequestDispatcher("/admin/deleteCar.jsp").forward(req, resp);
         }
-
-        req.getRequestDispatcher("/admin/deleteCar.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String id = req.getParameter("id");
-        if (id.isEmpty()) {
+        if (id == null) {
+            req.getRequestDispatcher("/admin/deleteCar.jsp").forward(req, resp);
+        } else if (id.isEmpty()) {
             req.setAttribute("error", errorMessage());
-
-        }
-        Car car = dao.getCar(Integer.parseInt(id));
-
-        if (car != null) {
-            dao.deleteCar(Integer.parseInt(id));
+            req.getRequestDispatcher("/admin/deleteCar.jsp").forward(req, resp);
         } else {
-            req.setAttribute("error", errorMessage());
-
+            Car car = dao.getCar(Integer.valueOf(id));
+            if (car != null) {
+                dao.deleteCar(Integer.valueOf(id));
+            } else {
+                req.setAttribute("error", errorMessage());
+            }
+            req.getRequestDispatcher("/admin/deleteCar.jsp").forward(req, resp);
         }
-        req.getRequestDispatcher("/admin/deleteCar.jsp").forward(req, resp);
     }
 
     public static String errorMessage() {
         String html1 = "<div class=\"alert alert-danger\" role=\"alert\">";
         String html2 = "</div>";
-        String errorData = " Id car incorrect! Please try again.";
+        String errorData = "Car id is incorrect! Please try again.";
         return html1 + errorData + html2;
     }
 }
--- src/main/webapp/admin/include/deleteCar.jsp
+++ src/main/webapp/admin/include/deleteCar.jsp
@@ -2,18 +2,18 @@
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <h2>Check car in database</h2>
 <form method="get" action="deleteCar">
-    Choose id: <input type="number" name="id" min="0">
+    Choose id: <input type="number" name="id" min="1">
     <button class="btn btn-info" type="submit">Check</button>
     <br/>
-    Car id:${id}<br/>
-    Make:${make}<br/>
-    Model:${model}<br/>
-    Year:${year}<br/>
+    Car id: ${car.id}<br/>
+    Make: ${car.make}<br/>
+    Model: ${car.model}<br/>
+    Year: ${car.year}<br/>
 </form>
 ${error}
 <br/>
 <h2>Delete car from database</h2>
 <form method="post" action="deleteCar">
-    Choose id: <input type="number" name="id" min="0">
+    Choose id: <input type="number" name="id" min="1">
     <button class="btn btn-danger" type="submit">Delete</button>
 </form>
