f322d1bc22dad87716f6ff26783f9bf286583ea1
arexe <arexepro@gmail.com>
2019-04-08 16:55:54 +0200

    Add LoginFilter and fix to LoginServlet

--- css-webapp/src/main/java/com/infoshare/academy/filters/AdminAuthenticationFilter.java
+++ css-webapp/src/main/java/com/infoshare/academy/filters/AdminAuthenticationFilter.java
@@ -1,42 +1,30 @@
 package com.infoshare.academy.filters;
 
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-@WebFilter("/admin/*")
+@WebFilter(filterName = "AdminAuthenticationFilter", urlPatterns = "/admin/*")
 public class AdminAuthenticationFilter implements Filter {
 
+    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         HttpSession session = httpRequest.getSession(false);
-        String loginURI = httpRequest.getContextPath() + "/login.jsp";
 
         boolean isLoggedIn = (session != null && session.getAttribute("username") != null);
 
         if (isLoggedIn && session.getAttribute("usertype").equals(1)) {
             chain.doFilter(request, response);
         } else {
+            String loginURI = httpRequest.getContextPath() + "/login";
             httpResponse.sendRedirect(loginURI);
         }
     }
-
-    public void destroy() {
-    }
-
-    public void init(FilterConfig fConfig) throws ServletException {
-    }
-
 }
