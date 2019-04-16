7d3aadf17fbf8786a2b67d656a71d948cafe8adf
Łukasz Waręcki <glenski2be7@gmail.com>
2019-04-16 21:53:55 +0200

    Add finding users by id

--- css-restapi/src/main/java/com/infoshare/academy/service/UserService.java
+++ css-restapi/src/main/java/com/infoshare/academy/service/UserService.java
@@ -1,12 +1,10 @@
 package com.infoshare.academy.service;
 
 import com.infoshare.academy.dao.UsersRepositoryDao;
+import com.infoshare.academy.domain.User;
 
 import javax.ejb.EJB;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -26,7 +24,20 @@ public class UserService {
     @GET
     @Path("/users")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getCarList() {
+    public Response getUserList() {
         return Response.ok(userDao.getUsersList()).build();
     }
+
+    @GET
+    @Path("/users/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUserById(@PathParam("id") int id) {
+        User user = userDao.getUserById(id);
+        if (user != null) {
+            return Response.ok(userDao.getUserById(id)).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+
 }
