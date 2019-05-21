0e5de6b2058040af767d70993ccdaa1bd99f0d12
arexe <arexepro@gmail.com>
2019-05-21 10:30:28 +0200

    Add permissions for Users and Reservations REST services

--- css-restapi/src/main/java/com/infoshare/academy/service/ReservationService.java
+++ css-restapi/src/main/java/com/infoshare/academy/service/ReservationService.java
@@ -1,6 +1,5 @@
 package com.infoshare.academy.service;
 
-
 import com.infoshare.academy.dao.CarsRepositoryDao;
 import com.infoshare.academy.dao.ReservationRepositoryDao;
 import com.infoshare.academy.dao.UsersRepositoryDao;
@@ -9,6 +8,7 @@ import com.infoshare.academy.domain.Reservation;
 import com.infoshare.academy.domain.User;
 import org.jboss.logging.Logger;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.EJB;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -16,36 +16,38 @@ import javax.ws.rs.core.Response;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * CarSharingSystem service for Reservations
+ */
 @Path("/")
 public class ReservationService {
 
+    private static Logger LOGGER = Logger.getLogger(ReservationService.class.getName());
+
     @EJB
     private ReservationRepositoryDao reservationDao;
-
     @EJB
     private CarsRepositoryDao carDao;
-
     @EJB
     private UsersRepositoryDao userDao;
 
-    public static Logger LOGGER = Logger.getLogger(ReservationService.class.getName());
-
+    @RolesAllowed("1")
     @GET
     @Path("/reservations")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getReservationList() {
-
         return Response.ok(reservationDao.list()).build();
     }
 
-
+    @RolesAllowed("1")
     @GET
-    @Path("/reservation")
+    @Path("/reservations")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getReservationById(@QueryParam("id") Integer id) {
         return Response.ok(reservationDao.getReservationById(id)).build();
     }
 
+    @RolesAllowed({"1","0"})
     @GET
     @Path("/availableAllCar")
     @Produces(MediaType.APPLICATION_JSON)
@@ -56,8 +58,7 @@ public class ReservationService {
         if (isPast(start(startDate)) || isAfter(start(startDate), end(endDate))) {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
-        LOGGER.info("Get available car form:" + startDate + " to " + endDate);
-
+        LOGGER.info("Get available car form: " + startDate + " to " + endDate);
         List<Car> cars = reservationDao.getCarListAvailableCar(start(startDate), end(endDate));
 
         if (cars != null) {
@@ -66,6 +67,7 @@ public class ReservationService {
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
+    @RolesAllowed({"1","0"})
     @GET
     @Path("/availableCar")
     @Produces(MediaType.APPLICATION_JSON)
@@ -78,14 +80,13 @@ public class ReservationService {
         if (isPast(start(startDate)) || isAfter(start(startDate), end(endDate))) {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
-
-        LOGGER.info("Get available car form:" + startDate + " to " + endDate);
-
+        LOGGER.info("Get available car form: " + startDate + " to " + endDate);
         List<Car> cars = reservationDao.getCarListAvailableCarLimit(start(startDate), end(endDate), currentPage, pageSize);
 
         return Response.ok(cars).build();
     }
 
+    @RolesAllowed("1")
     @GET
     @Path("/reservationUser")
     @Produces(MediaType.APPLICATION_JSON)
@@ -100,11 +101,11 @@ public class ReservationService {
             return Response.ok(reservationDao.reservationListByUserIdLimit(id, currentPage, pageSize)).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
-
     }
 
+    @RolesAllowed("1")
     @POST
-    @Path("/reservation")
+    @Path("/reservations")
     @Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
     @Produces(MediaType.APPLICATION_JSON)
     public Response addReservation(
@@ -133,8 +134,9 @@ public class ReservationService {
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
+    @RolesAllowed("1")
     @DELETE
-    @Path("/reservation")
+    @Path("/reservations")
     @Produces(MediaType.APPLICATION_JSON)
     public Response deleteReservation(
             @QueryParam("id") String idString) {
@@ -154,22 +156,21 @@ public class ReservationService {
         return Response.status(Response.Status.NOT_FOUND).build();
     }
 
-
-    public LocalDate start(String startDate) {
+    private LocalDate start(String startDate) {
         return LocalDate.parse(startDate);
     }
 
-    public LocalDate end(String endDate) {
+    private LocalDate end(String endDate) {
         return LocalDate.parse(endDate);
     }
 
-    public boolean isAfter(LocalDate startDate, LocalDate endDate) {
+    private boolean isAfter(LocalDate startDate, LocalDate endDate) {
         return startDate.isAfter(endDate);
     }
 
     private final LocalDate now = LocalDate.now();
 
-    public boolean isPast(LocalDate startDate) {
+    private boolean isPast(LocalDate startDate) {
         return now.isAfter(startDate);
     }
 }
